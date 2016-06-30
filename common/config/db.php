79ab79e1dd1aa59c2674d35e0c45c3efa0cb870b
Rat-Kat <kratush@gmail.com>
2016-06-30 18:55:30 +0300

    Delete db.php

--- common/config/db.php
+++
@@ -1,8 +0,0 @@
-<?
-
-return  [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=kratu155_gricajschool',
-    'username' => 'kratu155_kratush',
-    'password' => 'Kratush1984'
-];
